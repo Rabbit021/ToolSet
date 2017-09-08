4aa434feff3bfc7dcd808f5c123400051673d28f
wangheng <635253084@qq.com>
2017-09-08 14:48:49 +0800

    添加Excel转换库

---
+++ ExcelConverter/ExcelConverterMoudle.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mef.Modularity;
+using Prism.Modularity;
+using ToolSetsCore;
+
+namespace ExcelConverter
+{
+    [ModuleExport(typeof(ExcelConverterMoudle ))]
+    public class ExcelConverterMoudle : ToolSetModule
+    {
+        public override void RegisterPlugins()
+        {
+            base.RegisterPlugins();
+            PublicDatas.Instance.AddPlugins(new PluginItem { Header = "Excel转换", ViewName = "ExcelConverterView" });
+        }
+    }
+}
---
+++ ExcelConverter/Views/ExcelConverterView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+using ExcelConverter.ViewModels;
+using ToolSetsCore;
+
+namespace ExcelConverter.Views
+{
+    [Export]
+    [Export("ExcelConverterView", typeof(ExcelConverterView))]
+    public partial class ExcelConverterView : ToolSetViewBase
+    {
+        public ExcelConverterView()
+        {
+            InitializeComponent();
+        }
+
+        [Import]
+        public ExcelConverterVM ImportVM
+        {
+            set => VM = value;
+        }
+    }
+}
